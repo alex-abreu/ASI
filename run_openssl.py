9e4672d266561c5042980553b7d7a22d05f5d93e
Vitor Teixeira <vitor.teixeira267@gmail.com>
2020-09-08 09:16:11 -0300

    openssl run

---
+++ run_openssl.py
@@ -0,0 +1,64 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import os
+import sys
+import io
+import glob
+
+
+# %%
+path = 'dicionários\\'
+nameList = []
+for file in glob.glob(path+'*.txt'):
+    nameList += open(file, 'r').read().split('\n')
+
+
+
+# %%
+for i in range(0, len(nameList)):
+   nameList[i] = nameList[i].strip()
+nameList = list(set(nameList))
+nameList.sort()
+
+
+# %%
+fileList = [f"file{i}.enc" for i in range(1,31)]
+
+
+# %%
+fileList
+
+
+# %%
+decrypted = [''] * 30
+
+
+# %%
+
+#para cada senha em nameList
+for i in range(0, len(nameList)):
+   # para cada arquivo criptografado
+   for j in range(0,30):
+       #se o arquivo atual já foi decifrado
+       if decrypted[j] != '':
+           continue
+       #comando cmd pra decifrar
+       os.system(f'cmd /c "openssl enc -d -aes-256-cbc -pbkdf2 -salt -in {fileList[j]} -out decifrado{j}.txt -pass pass:{nameList[i]}') 
+       #nome do arquivo de saída(pode ser o arquivo decifrado ou não)
+       decFile = f"decifrado{j+1}.txt"
+       #abre o arquivo para leitura(caso exista). Caso esteja vazio, o arquivo original não foi               #decifrado. Caso contrário, ele foi decifrado.
+       try:
+           with open(decFile, 'r') as file:
+               if file.read() != '':
+                   decrypted[j] = nameList[i]
+                   print(f"Opa! Conseguimos decifrar um arquivo! O arquivo é {fileList[j]}, e a senha é: {nameList[i]}\n")
+       except(FileNotFoundError):
+           continue
+
+
+
+# %%
+
+
+
